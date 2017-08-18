3dedccfa16683d1a40618c282502e876f3579a92
Felix Plymouth <f.plymouth@gmail.com>
2017-08-18 14:46:10 +0200

    Removed default shows

--- src/server.js
+++ src/server.js
@@ -3,41 +3,10 @@ const Promise = require('bluebird');
 // Constants
 const db_name = "database.sqlite"
 
-const tmp_shows = [
-    	{
-            identifier:"ggar",
-            type : "webcomic",
-            name : "Go Get A Roomie",
-            image_xpath:"//div[@id='cc-comicbody']//img",
-       	    next_xpath:"//div[@id='cc-comicbody']/a",
-            text_xpath : "//div[@class='cc-newsbody']",
-            base_url : "http://www.gogetaroomie.com/comic/catching-the-flusters"
-    	},
-        {
-            identifier:"gunn",
-            type : "webcomic",
-            name : "Gunnerkrigg Court",
-            image_xpath : "//img[@class='comic_image']",
-            next_xpath : "//a[./img[@src='http://www.gunnerkrigg.com/images/next_a.jpg']]",
-            base_url : "http://www.gunnerkrigg.com/?p=1856",
-            logo : "http://www.gunnerkrigg.com/images/title2.jpg"
-        }
-];
-
-add_tmp = function(shows) {	
-	return Promise.map(tmp_shows,(tmp_show)=>{
-		return config.get_show(tmp_show.identifier).then((show)=>{
-			if (!show) return config.add_new_show(tmp_show);
-			return show;
-		});
-	})
-	.then(config.get_shows);
-}
 
 start = function (db_name) {
 	return db.init(db_name)
 		.then(config.get_shows)
-        .then(add_tmp)
 		.then(app.start_all)
 		.then(config.get_shows)
 		.then(manager.start_watchers)
