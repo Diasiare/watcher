db8ea6a147e0f6bc1f7887b882daf09a7c1569ca
Felix Plymouth <f.plymouth@gmail.com>
2017-08-23 15:25:56 +0200

    Revrite manager so that it is not infinetly recursive. Hopefully this should stop our memmory leaks

--- src/downloaders/manager.js
+++ src/downloaders/manager.js
@@ -1,6 +1,7 @@
 
 
 const Promise = require('bluebird');
+var started = false;
 Promise.config({
     cancellation: true
 });
@@ -9,23 +10,22 @@ Promise.config({
 
 const current_watchers = {};
 
+
+
 watching_cycle = function (show) {
-	return imdown.download_sequence(show).delay(show.interval)
-		.then(watching_cycle);
-}
-
-
-start_watcher = function (show) {
-	var ef = (e)=>{
+	if (!current_watchers[show.identifier].p ||
+		!current_watchers[show.identifier].p.isPending())
+		current_watchers[show.identifier].p = imdown.download_sequence(show).catch((e)=>{
 		console.error("ERROR");
 		console.error(e);
 		console.error(show);
-		console.error("Restarting\n\n");
-		 //For the moment just log failures and restart.
-		return Promise.resolve(show).delay(show.interval).then(watching_cycle).catch(ef);
-	}
-	var watcher = watching_cycle(show).catch(ef);;
-	current_watchers[show.identifier] = watcher;
+	});
+}
+
+start_watcher = function (show) {
+	current_watchers[show.identifier] = {};
+	watching_cycle(show);
+	current_watchers[show.identifier].t = setInterval(watching_cycle,show.interval,show);
 	return show;
 }
 
@@ -33,12 +33,12 @@ add_watcher = function(show) {
 	return Promise.resolve(show)
 		.then(stop_watcher)
 		.then(start_watcher);
-	
 }
 
 stop_watcher = function(show) {
-	if (show.identifier in current_watchers) {
-		current_watchers[show.identifier].cancel();
+	if (show.identifier in current_watchers) {		
+		clearInterval(current_watchers[show.identifier].t);
+		current_watchers[show.identifier].p.cancel();	
 	}
 	return show;
 }
